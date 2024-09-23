2abb0ff7a8a359bad7dc81347f32181dfc4a9b92
eamonnk <eamonn_kelly@hotmail.com>
2024-09-23 07:13:20 +0100

    update

--- labs/exercises/week1-exercise-1-print-shape.py
+++ labs/exercises/week1-exercise-1-print-shape.py
@@ -8,7 +8,7 @@
 
 # -- SOLUTION --
 # Using print command to display square shape composed of asterisks
-# and using \n escape sequence to wrap to a new line to allow 
-# it is done on a single line to allow it execute in a single print function 
+# and using \n escape sequence to wrap to a new line to allow output 
+# to be completed in a single print function 
 # rather than multiple print commands, making it more efficient
 print("**********\n*        *\n*        *\n*        *\n**********")
--- labs/exercises/week1-exercise-2-bmi.py
+++ labs/exercises/week1-exercise-2-bmi.py
@@ -2,23 +2,20 @@
 # Write a Python script that calculates the body-mass index. 
 
 # --Solution --
-# Prompt user to enter their name and assign that value to "name" object. It will be a string object
+# Prompt user to enter their name and assign that value to "name" variable. It will be a string object by default
 name = input("Please enter your name: ")
-print(type(name))
-# print(name)
-# Prompt  user to enter their weight and assign that value to "weight" object. Define the input value as an integer
+
+# Prompt user to enter their weight and assign that value to "weight" variable. Define the input value as an integer
 weight = int(input("Please enter your weight (Kgs): "))
-print(type(weight))
-print(weight)
-# Prompt user to enter their height and assign that value to "height" object. Define the input value as an integer
+
+# Prompt user to enter their height and assign that value to "height" variable. Define the input value as an integer
 height = int(input("Please enter your height (cm): "))
-print (height)
-print(type(height))
+
 
 # Calculate Body Mass Index(BMI), the weight in kilograms divided by the square of the height in meters,
 # we divide by 100 to convert cm to metres
-# the resultant value is a float, so we round that to two decimal places using the round function to give us a more user friendly value 
-# We assign that value to the variable "bmi"
+# Resultant value is a float. We round that to two decimal places using the round function for more user friendly value. 
+# Assign that value to the variable "bmi"
 bmi = str(round(weight/((height/100)**2), 2))
 print(bmi)
 print(type(bmi))
