ae785aae11b0f52d6212e046e072729f6f3fe0e7
eamonnk <eamonn_kelly@hotmail.com>
2024-09-23 09:47:04 +0100

    update from vsc update from vsc

--- labs/exercises/week1-exercise2-bmi.py
+++ labs/exercises/week1-exercise2-bmi.py
@@ -6,10 +6,10 @@
 # Prompt user to enter their name and assign that value to "name" variable. It will be a string object by default
 name = input("Please enter your name: ")
 
-# Prompt user to enter their weight and assign that value to "weight" variable. Define the input value as an integer
+# Prompt user to enter their weight and assign that value to "weight" variable. Convert the input value to an integer
 weight = int(input("Please enter your weight (Kgs): "))
 
-# Prompt user to enter their height and assign that value to "height" variable. Define the input value as an integer
+# Prompt user to enter their height and assign that value to "height" variable. Convert the input value to an integer
 height = int(input("Please enter your height (cm): "))
 
 
@@ -17,7 +17,8 @@ height = int(input("Please enter your height (cm): "))
 # we divide by 100 to convert cm to metres
 # Resultant value is a float. We round that to two decimal places using the round function for more user friendly value. 
 # Assign that value to the variable "bmi"
-bmi = str(round(weight/((height/100)**2), 2))
+bmi = round(weight/((height/100)**2), 2)
 
 # printing name variable and calculated bmi value with template string converting the name variable to upper case
+# converting bmi variable to str object, as is a float when defined earlier
 print("Thank You" +  " " + name.upper() + ":" + "Your Body Mass Index (bmi) is" + " " + str(bmi))
